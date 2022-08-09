6178b6cc3583957d0cea0c8dee9413adc92ddb83
Marcos Carmo Silva <mcarmo@youvr.local>
2022-08-09 14:04:22 -0300

    first commit

---
+++ componentes/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner(){
+  return(
+    <header className="banner">
+    <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+    </header>
+  ) //JSX --> É essas tag ai em cima que parece HTML mais não é.
+}
+
+export default Banner
